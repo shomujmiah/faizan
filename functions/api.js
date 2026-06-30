bae74b8a510baa9b7021ae4d4fb00e9a1bfb7446
shomujmiah <shomuj.miah@gmail.com>
2026-06-30 01:29:04 +0100

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -1,14 +1,20 @@
 export async function onRequest(context) {
-  // Replace this URL with your ACTUAL Google Script Web App URL
   const GOOGLE_URL = 'https://script.google.com/macros/s/AKfycbzCKtE-mKx_X8OZMsvihtXKea9eBiHxstyWIhPNdnBFoFSZnAnz374V08gpm32W_4g3/exec?action=getData';
 
-  const response = await fetch(GOOGLE_URL);
-  const data = await response.json();
-
-  return new Response(JSON.stringify(data), {
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*' // This makes the browser happy
+  try {
+    const response = await fetch(GOOGLE_URL);
+    if (!response.ok) {
+        throw new Error('Failed to fetch from Google');
     }
-  });
+    const data = await response.json();
+
+    return new Response(JSON.stringify(data), {
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      }
+    });
+  } catch (err) {
+    return new Response(JSON.stringify({ error: err.message }), { status: 500 });
+  }
 }
