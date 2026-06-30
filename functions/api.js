2505dbc8b98c97661aad63e1c04e32c643229482
shomujmiah <shomuj.miah@gmail.com>
2026-06-30 02:12:01 +0100

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -1,26 +1,32 @@
 export async function onRequest(context) {
+  const { request } = context;
   const GOOGLE_URL = 'https://script.google.com/macros/s/AKfycbzCKtE-mKx_X8OZMsvihtXKea9eBiHxstyWIhPNdnBFoFSZnAnz374V08gpm32W_4g3/exec?action=getData';
 
+  // Handle CORS Pre-flight
+  if (request.method === "OPTIONS") {
+    return new Response(null, {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type",
+      },
+    });
+  }
+
   try {
-    // Add a 10-second timeout to the fetch
-    const controller = new AbortController();
-    const timeout = setTimeout(() => controller.abort(), 10000);
-    
-    const response = await fetch(GOOGLE_URL, { signal: controller.signal });
-    clearTimeout(timeout);
-
-    if (!response.ok) {
-      return new Response("Google Script returned status: " + response.status, { status: 500 });
-    }
-
+    const response = await fetch(GOOGLE_URL);
     const data = await response.json();
+
     return new Response(JSON.stringify(data), {
       headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-      }
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*", // Allows any site to read the data
+      },
     });
   } catch (err) {
-    return new Response("Error: " + err.message, { status: 500 });
+    return new Response(JSON.stringify({ error: err.message }), { 
+      status: 500,
+      headers: { "Content-Type": "application/json" }
+    });
   }
 }
